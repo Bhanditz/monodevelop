fea06592da7ca4f6abb71e7007098a44ac93b30c
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2017-02-19 15:39:27 +0300

    cor debug changes

---
+++ main/src/addins/MonoDevelop.Debugger.Win32/CorApi2/Pinvoke/IDllLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PinvokeKit
+{
+    public interface IDllLoader
+    {
+        IntPtr LoadLibrary(string absoluteDllPath);
+        void FreeLibrary(IntPtr handle);
+        IntPtr GetProcAddress(IntPtr handle, string methodName);
+
+        bool IsLoaded(string absoluteDllPath);
+    }
+}
