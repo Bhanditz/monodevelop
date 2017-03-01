dd682fb4cd39c908eb61b909437c41276a1b0ac1
Mikhail Filippov <Mikhail.Filippov@jetbrains.com>
2017-03-01 19:40:24 +0300

    Add two debugger tests.

--- main/src/addins/MonoDevelop.Debugger.Win32/Mono.Debugging.Win32.Tests/ApplicationDescriptorEx.cs
+++ main/src/addins/MonoDevelop.Debugger.Win32/Mono.Debugging.Win32.Tests/ApplicationDescriptorEx.cs
@@ -1,50 +1,22 @@
-﻿using System;
-using System.Threading;
-using Mono.Debugging.Client;
-using Should;
+﻿using Mono.Debugging.Client;
 
 namespace Mono.Debugging.Win32.Tests
 {
     internal static class ApplicationDescriptorEx
     {
-        public static void ExecuteWhenDebuggerBreakHitted(this ApplicationDescriptor app, Action<DebuggerSession> action, TimeSpan timeout)
+        public static DebuggerStartInfo GetStartInfoForDebuggerBreak(this ApplicationDescriptor app)
         {
-            CorDebuggerSession session = null;
-            try
+            return new DebuggerStartInfo
             {
-                session = new CorDebuggerSession(new char[] { });
-
-                var startInfo = new DebuggerStartInfo
-                {
-                    Arguments = "BREAK",
-                    CloseExternalConsoleOnExit = true,
-                    Command = app.BinaryPath,
-                    RequiresManualStart = false,
-                    RuntimeArguments = "",
-                    UseExternalConsole = true,
-                    WorkingDirectory = app
-                        .WorkingDirectory
-                };
-
-                var timeGuard = new ManualResetEvent(false);
-
-                session.TargetInterrupted += new EventHandler<TargetEventArgs>((sender, args) =>
-                {
-                    action(session);
-                    timeGuard.Set();
-                });
-
-                session.Run(startInfo, new DebuggerSessionOptions());
-                timeGuard.WaitOne(timeout).ShouldBeTrue("Stoped event not happend");
-                session.Stop();
-            }
-            finally
-            {
-                if (session != null)
-                {
-                    session.Dispose();
-                }
-            }
+                Arguments = "BREAK",
+                CloseExternalConsoleOnExit = true,
+                Command = app.BinaryPath,
+                RequiresManualStart = false,
+                RuntimeArguments = "",
+                UseExternalConsole = true,
+                WorkingDirectory = app
+                    .WorkingDirectory
+            };
         }
     }
 }
--- main/src/addins/MonoDevelop.Debugger.Win32/Mono.Debugging.Win32.Tests/CorDebugSessionTests.cs
+++ main/src/addins/MonoDevelop.Debugger.Win32/Mono.Debugging.Win32.Tests/CorDebugSessionTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
+using Mono.Debugging.Client;
 using Should;
 using Xunit;
 
@@ -10,32 +12,78 @@ namespace Mono.Debugging.Win32.Tests
         [Fact]
         public void ShouldCorrectlyStopOnDebuggerBreak()
         {
-            Constants.Net45ConsoleApp.ExecuteWhenDebuggerBreakHitted(session =>
+            CorDebuggerSession session = null;
+            var app = Constants.Net45ConsoleApp;
+            try
             {
-                session.IsConnected.ShouldBeTrue();
-                session.IsRunning.ShouldBeFalse();
-            }, TimeSpan.FromSeconds(120));
+                session = new CorDebuggerSession(new char[] { });
+                var sessionStartGuard = new ManualResetEvent(false);
+                session.TargetInterrupted += (s1, a1) =>
+                {
+                    var corDebugSession = s1 as CorDebuggerSession;
+                    corDebugSession.ShouldNotBeNull();
+                    corDebugSession.IsConnected.ShouldBeTrue();
+                    corDebugSession.IsRunning.ShouldBeFalse();
+                    sessionStartGuard.Set();
+                };
+
+                session.Run(app.GetStartInfoForDebuggerBreak(), new DebuggerSessionOptions());
+                sessionStartGuard.WaitOne(TimeSpan.FromSeconds(10)).ShouldBeTrue("Session wasn't start");
+                session.Stop();
+            }
+            finally
+            {
+                if (session != null)
+                {
+                    session.Dispose();
+                    session = null;
+                }
+            }
         }
 
         [Fact]
         public void ShouldCorrectlyStopOnCutomBreakPoint()
         {
-            Constants.Net45ConsoleApp.ExecuteWhenDebuggerBreakHitted(session =>
+            CorDebuggerSession session = null;
+            var app = Constants.Net45ConsoleApp;
+            try
             {
-                var corDebugSession = session as CorDebuggerSession;
-
-                corDebugSession.ShouldNotBeNull();
-                var docFiles = corDebugSession.GetAllDocumentPaths();
-                foreach (var docFile in docFiles)
+                session = new CorDebuggerSession(new char[] { });
+                var breakPointHittedGuard = new ManualResetEvent(false);
+                session.TargetInterrupted += (s1, a1) =>
                 {
-                    if (Path.GetFileName(docFile) != "Program.cs")
-                        continue;
-                    session.Breakpoints.Add(docFile, 14);
-                    break;
-                }
-                session.Continue();
+                    var corDebugSession = s1 as CorDebuggerSession;
+                    corDebugSession.ShouldNotBeNull();
 
-            }, TimeSpan.FromSeconds(120));
+                    var docFiles = corDebugSession.GetAllDocumentPaths();
+                    foreach (var docFile in docFiles)
+                    {
+                        if (Path.GetFileName(docFile) != "Program.cs")
+                            continue;
+                        corDebugSession.Breakpoints.Add(docFile, 14);
+                        break;
+                    }
+                    corDebugSession.IsConnected.ShouldBeTrue();
+                    corDebugSession.IsRunning.ShouldBeFalse();
+
+                    corDebugSession.TargetHitBreakpoint += (s2, a2) =>
+                    {
+                        breakPointHittedGuard.Set();
+                    };
+                    corDebugSession.Continue();
+                };
+
+                session.Run(app.GetStartInfoForDebuggerBreak(), new DebuggerSessionOptions());
+                breakPointHittedGuard.WaitOne(TimeSpan.FromSeconds(10)).ShouldBeTrue("Breakpoint wasn't hit");
+                session.Stop();
+            }
+            finally
+            {
+                if (session != null)
+                {
+                    session.Dispose();
+                }
+            }
         }
 
     }
