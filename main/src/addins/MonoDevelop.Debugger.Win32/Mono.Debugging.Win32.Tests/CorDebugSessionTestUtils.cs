2426aca020807292390ad77adf464e63062adb3b
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2017-03-02 12:37:15 +0300

    implemented toggle breakpoint helper

--- main/src/addins/MonoDevelop.Debugger.Win32/Mono.Debugging.Win32.Tests/CorDebugSessionTestUtils.cs
+++ main/src/addins/MonoDevelop.Debugger.Win32/Mono.Debugging.Win32.Tests/CorDebugSessionTestUtils.cs
@@ -24,5 +24,35 @@ namespace Mono.Debugging.Win32.Tests
                 session.TargetStopped -= sessionOnTargetStopped;
             }
         }
+
+        public static void ToggleBreakpointAndWaitForBind (this CorDebuggerSession session, string filename, int line,
+            int timeoutSeconds = 10)
+        {
+            var toggleBreakpointHelper = new ManualResetEvent (false);
+            EventHandler<BreakEventArgs> sessionOnBreakpointStatusChanged = (sender, args) => {
+                if (args == null)
+                    return;
+
+                var breakpoint = args.BreakEvent as Breakpoint;
+                if (breakpoint == null)
+                    return;
+
+                if (!breakpoint.FileName.Equals (filename) || line != breakpoint.Line) //TODO: normalize filename paths
+                    return;
+
+                if (breakpoint.GetStatus (session) == BreakEventStatus.Bound)
+                    toggleBreakpointHelper.Set ();
+            };
+
+            session.Breakpoints.BreakEventStatusChanged += sessionOnBreakpointStatusChanged;
+            try {
+                session.Breakpoints.Add (filename, line);
+                toggleBreakpointHelper.WaitOne (TimeSpan.FromSeconds (timeoutSeconds)).ShouldBeTrue
+                    (string.Format("Breakpoint {0}:{1} wasn't bound", filename, line));
+            }
+            finally {
+                session.Breakpoints.BreakEventStatusChanged -= sessionOnBreakpointStatusChanged;
+            }
+        }
     }
 }
