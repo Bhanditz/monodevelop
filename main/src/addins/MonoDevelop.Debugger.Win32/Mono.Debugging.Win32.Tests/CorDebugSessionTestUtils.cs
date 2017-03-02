d720c0b7e6eda8fee5679a379a75e19485dfbaa8
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2017-03-02 12:19:23 +0300

    implemented wait for session stop helper

---
+++ main/src/addins/MonoDevelop.Debugger.Win32/Mono.Debugging.Win32.Tests/CorDebugSessionTestUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Mono.Debugging.Client;
+using Should;
+
+namespace Mono.Debugging.Win32.Tests
+{
+    public static class CorDebugSessionTestUtils
+    {
+        private static EventHandler<TargetEventArgs> sessionOnTargetStopped;
+
+        public static void StopAndWait (this CorDebuggerSession session, int timeoutSeconds = 10)
+        {
+            var stopHelper = new ManualResetEvent (false);
+            sessionOnTargetStopped = (sender, args) => {
+                stopHelper.Set ();
+            };
+            session.TargetStopped += sessionOnTargetStopped;
+            try {
+                session.Stop ();
+                stopHelper.WaitOne (TimeSpan.FromSeconds (timeoutSeconds)).ShouldBeTrue ("Session wasn't stopped");
+            }
+            finally {
+                session.TargetStopped -= sessionOnTargetStopped;
+            }
+        }
+    }
+}
