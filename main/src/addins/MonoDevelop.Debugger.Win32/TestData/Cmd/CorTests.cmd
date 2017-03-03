5ec3bf77e0bfde77be373225395942f5e718b969
hypersw <baltic@hypersw.net>
2017-03-03 05:51:46 +0300

    Test runner win+ubuntu.

---
+++ main/src/addins/MonoDevelop.Debugger.Win32/TestData/Cmd/CorTests.cmd
@@ -0,0 +1,12 @@
+#!/bin/sh 
+
+GOTO WIN
+
+dp0=$(cd "$(dirname "$0")"; pwd)
+mono $dp0/../Tools/NUnit/3.6/nunit3-console.exe $dp0/../../CorApi.Tests/bin/Debug/CorApi.Tests.dll --inprocess --where:cat==Core --noresult
+
+exit
+
+:WIN
+
+%~dp0\..\Tools\Mono\v4.8_win\bin\mono.exe %~dp0\..\Tools\NUnit\3.6\nunit3-console.exe %~dp0\..\..\CorApi.Tests\bin\Debug\CorApi.Tests.dll --inprocess --where:cat==Core --noresult
