f7b99516217a9b88864a35549a064a17b9559399
hypersw <baltic@hypersw.net>
2017-03-03 19:33:45 +0300

    Rig for running off the debug build of the coreclr.

--- main/src/addins/MonoDevelop.Debugger.Win32/CorApi.Tests/Src/Infra/CorClrApplicationDescriptorEx.cs
+++ main/src/addins/MonoDevelop.Debugger.Win32/CorApi.Tests/Src/Infra/CorClrApplicationDescriptorEx.cs
@@ -9,26 +9,24 @@ namespace CorApi.Tests
 {
     internal static class CorClrApplicationDescriptorEx
     {
-        private static readonly string DotNetCoreRootPath = PlatformUtil.IsRunningUnderWindows ? Path.Combine(Constants.TestDataFolder, "Tools", "NetCore", "v1.0.3_win")+"\\": @"/usr/share/dotnet/";
-        private static readonly string PlatformVersion = "1.0.3";
+        private static readonly string DotNetCoreRootPath = PlatformUtil.IsRunningUnderWindows ? Path.Combine(Constants.TestDataFolder, "Tools", "NetCore", /*"v1.0.3_win"*/"v1.0.5_win_mfdebug")+"\\": @"/usr/share/dotnet/";
+        private static readonly string PlatformVersion = "1.0.5";
 
         public static string GetCommandlineForDebuggerBreak(this ApplicationDescriptor app)
         {
-            return string.Format("\"{0}{1}\" \"{2}\" BREAK", DotNetCoreRootPath, DotNetCliExeName, app.BinaryPath);
+            return $"\"{DotNetCoreRootPath}{DotNetCliExeName}\" \"{app.BinaryPath}\" BREAK";
         }
 
         public static string GetCommandlineForSleep(this ApplicationDescriptor app, TimeSpan sleepTime)
         {
-            return string.Format("\"{0}{1}\" \"{2}\" SLEEP {3}", DotNetCoreRootPath, DotNetCliExeName,
-                app.BinaryPath, sleepTime.TotalMilliseconds);
+            return $"\"{DotNetCoreRootPath}{DotNetCliExeName}\" \"{app.BinaryPath}\" SLEEP {sleepTime.TotalMilliseconds}";
         }
 
-        private static readonly string DotNetCliExeName = PlatformUtil.IsRunningUnderWindows ? "dotnet.exe" : "dotnet";
+        private static readonly string DotNetCliExeName = PlatformUtil.IsRunningUnderWindows ? /*"dotnet.exe"*/$"shared/Microsoft.NETCore.App/{PlatformVersion}/CoreRun.exe" : "dotnet";
 
         public static string GetDbgShimPath(this ApplicationDescriptor app)
         {
-            return string.Format(@"{0}shared/Microsoft.NETCore.App/{1}/{2}", DotNetCoreRootPath, PlatformVersion,
-                PlatformUtil.IsRunningUnderWindows ? "dbgshim.dll" : "libdbgshim.dll");
+            return $@"{DotNetCoreRootPath}shared/Microsoft.NETCore.App/{PlatformVersion}/{(PlatformUtil.IsRunningUnderWindows ? "dbgshim.dll" : "libdbgshim.dll")}";
         }
     }
 }
