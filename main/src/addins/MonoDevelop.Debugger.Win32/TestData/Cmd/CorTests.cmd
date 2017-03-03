777be4f814eed83fde6007802b5141ac0e035f81
hypersw <baltic@hypersw.net>
2017-03-03 18:31:35 +0300

    Fix killing the terminal.

--- main/src/addins/MonoDevelop.Debugger.Win32/TestData/Cmd/CorTests.cmd
+++ main/src/addins/MonoDevelop.Debugger.Win32/TestData/Cmd/CorTests.cmd
@@ -5,7 +5,7 @@ GOTO WIN
 dp0=$(cd "$(dirname "$0")"; pwd)
 mono $dp0/../Tools/NUnit/3.6/nunit3-console.exe $dp0/../../CorApi.Tests/bin/Debug/CorApi.Tests.dll --inprocess --where:cat==Core --noresult
 
-exit
+return
 
 :WIN
 
